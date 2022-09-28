1d815e57a57dcce9b3e6849bb3b48cce98116c1a
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-09-29 00:17:44 +0100

    codegen: add code gene symbol table scanning
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- codegen/src/lib.rs
+++ codegen/src/lib.rs
@@ -1,16 +1,22 @@
 ///! Take AST (-> Verify the validity of the message)? -> Generate code
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+pub mod codegen;
 
 #[cfg(test)]
-mod tests {
-    use super::*;
+mod test {
+    use frontend_csv::parser::parser::Parser;
+    use frontend_csv::scanner::scanner::Scanner;
+    use std::fs;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn init_code_py_gen() {
+        let path_file = std::env::var_os("CSV_PATH").unwrap();
+        let contents = fs::read_to_string(format!("{}/bolt1.csv", path_file.to_str().unwrap()))
+            .expect("Something went wrong reading the file");
+        let mut scanner = Scanner::new();
+        let mut parser = Parser::new();
+        let char_vec: Vec<char> = contents.chars().collect();
+        let tokens = scanner.scan(&char_vec);
+        parser.parse(&tokens);
+        // FIXIME: init python codegene
     }
 }
