7894cb417e3fc528918160ce5bc6b65600ab5850
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-10-03 17:15:13 +0200

    codegen: add python implementation
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- codegen/src/lib.rs
+++ codegen/src/lib.rs
@@ -1,11 +1,15 @@
 ///! Take AST (-> Verify the validity of the message)? -> Generate code
 pub mod codegen;
+pub mod python;
 
 #[cfg(test)]
 mod test {
     use frontend_csv::parser::parser::Parser;
     use frontend_csv::scanner::scanner::Scanner;
     use std::fs;
+
+    use crate::codegen::CodeGen;
+    use crate::python::PythonCodeGen;
 
     #[test]
     fn init_code_py_gen() {
@@ -17,6 +21,9 @@ mod test {
         let char_vec: Vec<char> = contents.chars().collect();
         let tokens = scanner.scan(&char_vec);
         parser.parse(&tokens);
-        // FIXIME: init python codegene
+        let mut python_gen = PythonCodeGen::new(&parser.symbol_table);
+        python_gen.generate(&parser.symbol_table);
+        println!("------> {}", python_gen.file_content);
+        assert!(python_gen.file_content.contains("InitMsg"));
     }
 }
