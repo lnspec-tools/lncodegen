52bbffbc999448c5098142446fcb800ac186e4aa
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-10-11 02:34:57 +0200

    codegen: improve the interface of the codegen
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- codegen/src/lib.rs
+++ codegen/src/lib.rs
@@ -55,4 +55,28 @@ mod test {
         trace!("\n{}", python_gen.file_content);
         assert!(python_gen.file_content.contains("InitMsg"));
     }
+
+    #[test]
+    fn init_code_py_gen_with_tlv_stream() {
+        init();
+        let contents = "msgtype,init,16\n \
+                        msgdata,init,gflen,u16,\n \
+                       msgdata,init,globalfeatures,byte,gflen\n \
+                       msgdata,init,flen,u16,\n \
+                       msgdata,init,features,byte,flen\n \
+                       tlvtype,init_tlvs,networks,1\n \
+                       tlvdata,init_tlvs,networks,chains,chain_hash,...\n \
+                       tlvtype,init_tlvs,remote_addr,3\n \
+                       tlvdata,init_tlvs,remote_addr,data,byte,...\n";
+        trace!("{}", contents);
+        let mut scanner = Scanner::new();
+        let mut parser = Parser::new();
+        let char_vec: Vec<char> = contents.chars().collect();
+        let tokens = scanner.scan(&char_vec);
+        parser.parse(&tokens);
+        let mut python_gen = PythonCodeGen::new(&parser.symbol_table);
+        python_gen.generate(&parser.symbol_table);
+        trace!("\n{}", python_gen.file_content);
+        assert!(python_gen.file_content.contains("InitMsg"));
+    }
 }
