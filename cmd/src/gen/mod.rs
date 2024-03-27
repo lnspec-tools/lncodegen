f54772124542159cbc94ba72ff9d8b85ce2c9ce5
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2024-03-27 11:19:08 +0100

    cmd: use stable rust
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- cmd/src/gen/mod.rs
+++ cmd/src/gen/mod.rs
@@ -1,30 +1,10 @@
 //! Gen logic to dispach the different way to generate
 //! the lightning network spec.
-use std::fmt::Display;
 pub(crate) mod csv_method;
 
 pub trait CodeGenMethod {
-    type Error = CodeGenError;
     /// Take the content of the file where the bolt
     /// is defined and generate the python code that contains
     /// all the message define inside it
-    async fn generate(&self, bolt_content: &str) -> Result<String, Self::Error>;
-}
-
-pub struct CodeGenError {
-    cause: String,
-}
-
-impl CodeGenError {
-    pub fn new(cause: &str) -> Self {
-        CodeGenError {
-            cause: cause.to_owned(),
-        }
-    }
-}
-
-impl Display for CodeGenError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "codegen error: {}", self.cause)
-    }
+    fn generate(&self, bolt_content: &str) -> anyhow::Result<String>;
 }
--- cmd/src/main.rs
+++ cmd/src/main.rs
@@ -1,76 +1,33 @@
-#![allow(incomplete_features)]
-#![feature(async_fn_in_trait)]
-#![feature(associated_type_defaults)]
-use std::fmt::Display;
+use std::fs;
 
 use clap::Parser;
-use log::debug;
 use radicle_term as term;
-use rio_rt::runitime as rio;
 
 mod cmd;
 mod gen;
 
-use cmd::cmd_args::{Cli, Commands};
-use gen::{csv_method::CSVCodeGen, CodeGenMethod};
+use crate::cmd::cmd_args::{Cli, Commands};
+use crate::gen::csv_method::CSVCodeGen;
+use crate::gen::CodeGenMethod;
 
-#[derive(Debug, Clone)]
-struct DispachError {
-    cause: String,
-}
-
-impl DispachError {
-    pub fn new(cause: &str) -> Self {
-        DispachError {
-            cause: cause.to_owned(),
-        }
-    }
-}
-
-impl Display for DispachError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.cause)
-    }
-}
-
-impl From<std::io::Error> for DispachError {
-    fn from(value: std::io::Error) -> Self {
-        DispachError {
-            cause: format!("{value}"),
-        }
-    }
-}
-
-impl From<crate::gen::CodeGenError> for DispachError {
-    fn from(value: crate::gen::CodeGenError) -> Self {
-        DispachError {
-            cause: value.to_string(),
-        }
-    }
-}
-
-async fn dispach_cmd(args: &Cli) -> Result<(), DispachError> {
+fn dispach_cmd(args: &Cli) -> anyhow::Result<()> {
     match &args.command {
         Commands::Generate {
             bolt,
             to: result_path,
         } => {
-            if let Err(err) = async_std::fs::metadata(bolt).await {
-                return Err(DispachError::new(format!("error: {err}").as_str()));
-            }
-            let file_content = async_std::fs::read_to_string(bolt).await?;
+            fs::metadata(bolt)?;
+            let file_content = fs::read_to_string(bolt)?;
             let lang = args.lang.clone().unwrap();
             let generator = CSVCodeGen {
                 lang: lang.to_owned(),
             };
-            let result = generator.generate(file_content.as_str()).await?;
+            let result = generator.generate(file_content.as_str())?;
             term::success!(
                 "Generate {lang} to {}",
                 result_path.as_os_str().to_str().unwrap()
             );
-            if let Err(err) = async_std::fs::write(result_path, result).await {
-                return Err(DispachError::from(err));
-            }
+            fs::write(result_path, result)?;
             Ok(())
         }
         Commands::Decode { from } => {
@@ -78,23 +35,19 @@ async fn dispach_cmd(args: &Cli) -> Result<(), DispachError> {
             use fundamentals::core::FromWire;
             use std::io::BufReader;
 
-            let bytes = hex::decode(from).expect("error while decoding hex to bytes");
+            let bytes = hex::decode(from)?;
             let mut reader = BufReader::new(bytes.as_slice());
             let init = Init::from_wire(&mut reader)?;
-            term::success!("Done\n{:#?}", init);
+            term::success!("{:#?}", init);
             Ok(())
         }
     }
 }
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     env_logger::init();
-    debug!("running lncodegen.rs");
+
     let args = Cli::parse();
-    rio::block_on(async move {
-        if let Err(err) = dispach_cmd(&args).await {
-            term::error(format!("runtime error: {err}"));
-        }
-    });
-    rio::wait();
+    dispach_cmd(&args)?;
+    Ok(())
 }
